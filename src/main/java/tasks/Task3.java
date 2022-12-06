2611066d657bd8a4c15bdb3984c2103e90585421
Aleksandr Vasilyev <ap.vasilyev@yandex.ru>
2022-12-07 02:26:12 +0300

    refactoring: collection and stream tasks

--- src/main/java/tasks/Task3.java
+++ src/main/java/tasks/Task3.java
@@ -1,17 +1,26 @@
 package tasks;
 
 import common.Person;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 /*
 Задача 3
-Отсортировать коллекцию сначала по фамилии, по имени (при равной фамилии), и по дате создания (при равных фамилии и имени)
+Отсортировать коллекцию сначала по фамилии,
+по имени (при равной фамилии),
+и по дате создания (при равных фамилии и имени)
  */
 public class Task3 {
 
   public static List<Person> sort(Collection<Person> persons) {
-    return new ArrayList<>(persons);
+    return persons.stream().sorted((person1, person2) -> {
+      if (!person1.getSecondName().equals(person2.getSecondName())) {
+        return person1.getSecondName().compareTo(person2.getSecondName());
+      } else if (!person1.getFirstName().equals(person2.getFirstName())) {
+        return person1.getFirstName().compareTo(person2.getFirstName());
+      } else {
+        return person1.getCreatedAt().compareTo(person2.getCreatedAt());
+      }
+    }).toList();
   }
 }
--- src/main/java/tasks/Task6.java
+++ src/main/java/tasks/Task6.java
@@ -3,22 +3,28 @@ package tasks;
 import common.Area;
 import common.Person;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /*
 Имеются
 - коллекция персон Collection<Person>
 - словарь Map<Integer, Set<Integer>>, сопоставляющий каждой персоне множество id регионов
 - коллекция всех регионов Collection<Area>
-На выходе хочется получить множество строк вида "Имя - регион". Если у персон регионов несколько, таких строк так же будет несколько
+На выходе хочется получить множество строк вида "Имя - регион".
+Если у персон регионов несколько, таких строк так же будет несколько
  */
 public class Task6 {
 
   public static Set<String> getPersonDescriptions(Collection<Person> persons,
                                                   Map<Integer, Set<Integer>> personAreaIds,
                                                   Collection<Area> areas) {
-    return new HashSet<>();
+    Map<Integer, String> areasNames = areas.stream()
+            .collect(Collectors.toMap(Area::getId, Area::getName));
+    return persons.stream()
+            .flatMap(person -> personAreaIds.get(person.getId()).stream()
+                    .map(areaId -> person.getFirstName() + " - " + areasNames.get(areaId)))
+            .collect(Collectors.toSet());
   }
 }
