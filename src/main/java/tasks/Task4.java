395c644657d536bbeccb1d9c96f1f7edd6825f6b
bannovdaniil <baddan@mail.ru>
2022-12-07 20:03:02 +0300

    task4: Список персон класса Person необходимо сконвертировать в список ApiPersonDto (предположим, что это некоторый внешний формат) Конвертер для одной персоны - personConverter.convert()

--- src/main/java/tasks/Task4.java
+++ src/main/java/tasks/Task4.java
@@ -3,25 +3,21 @@ package tasks;
 import common.ApiPersonDto;
 import common.Person;
 import common.PersonConverter;
-import java.util.ArrayList;
+
 import java.util.List;
 
-/*
-Задача 4
-Список персон класса Person необходимо сконвертировать в список ApiPersonDto
-(предположим, что это некоторый внешний формат)
-Конвертер для одной персоны - personConverter.convert()
-FYI - DTO = Data Transfer Object - распространенный паттерн, можно погуглить
- */
 public class Task4 {
 
-  private final PersonConverter personConverter;
+    private final PersonConverter personConverter;
 
-  public Task4(PersonConverter personConverter) {
-    this.personConverter = personConverter;
-  }
+    public Task4(PersonConverter personConverter) {
+        this.personConverter = personConverter;
+    }
 
-  public List<ApiPersonDto> convert(List<Person> persons) {
-    return new ArrayList<>();
-  }
+    public List<ApiPersonDto> convert(List<Person> persons) {
+
+        return persons.stream()
+                .map(personConverter::convert)
+                .toList();
+    }
 }
