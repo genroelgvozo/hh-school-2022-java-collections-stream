cbd90b2a1bdf8f36637c64b3443704f94f8405f3
bannovdaniil <baddan@mail.ru>
2022-12-07 19:46:14 +0300

    task2: На вход принимаются две коллекции объектов Person и величина limit Необходимо объединить обе коллекции отсортировать персоны по дате создания и выдать первые limit штук.

--- src/main/java/tasks/Task2.java
+++ src/main/java/tasks/Task2.java
@@ -1,21 +1,22 @@
 package tasks;
 
 import common.Person;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
-/*
-Задача 2
-На вход принимаются две коллекции объектов Person и величина limit
-Необходимо объеденить обе коллекции
-отсортировать персоны по дате создания и выдать первые limit штук.
- */
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Task2 {
 
-  public static List<Person> combineAndSortWithLimit(Collection<Person> persons1,
-                                                     Collection<Person> persons2,
-                                                     int limit) {
-    return new ArrayList<>();
-  }
+    public static List<Person> combineAndSortWithLimit(Collection<Person> persons1,
+                                                       Collection<Person> persons2,
+                                                       int limit) {
+
+        return Stream.concat(persons1.stream(), persons2.stream())
+                .distinct()
+                .sorted(Comparator.comparing(Person::getCreatedAt))
+                .limit(limit)
+                .toList();
+    }
 }
