c289a751ce37b1004cd8a3d47504c3f957cb34cc
abdulsaidov <aa.abdulsaidov@gmail.com>
2022-12-07 17:27:45 +0530

    resolved 4th task

--- src/main/java/tasks/Task4.java
+++ src/main/java/tasks/Task4.java
@@ -3,8 +3,9 @@ package tasks;
 import common.ApiPersonDto;
 import common.Person;
 import common.PersonConverter;
-import java.util.ArrayList;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
 Задача 4
@@ -15,13 +16,16 @@ FYI - DTO = Data Transfer Object - распространенный паттер
  */
 public class Task4 {
 
-  private final PersonConverter personConverter;
+    private final PersonConverter personConverter;
 
-  public Task4(PersonConverter personConverter) {
-    this.personConverter = personConverter;
-  }
+    public Task4(PersonConverter personConverter) {
+        this.personConverter = personConverter;
+    }
 
-  public List<ApiPersonDto> convert(List<Person> persons) {
-    return new ArrayList<>();
-  }
+    public List<ApiPersonDto> convert(List<Person> persons) {
+
+        return persons.stream()
+                .map(personConverter::convert)
+                .collect(Collectors.toList());
+    }
 }
