515cb74e575b32a6877f041087754ca01cbfc7f2
abdulsaidov <aa.abdulsaidov@gmail.com>
2022-12-07 16:12:09 +0530

    resolved 2nd task

--- src/main/java/tasks/Task2.java
+++ src/main/java/tasks/Task2.java
@@ -1,9 +1,12 @@
 package tasks;
 
 import common.Person;
-import java.util.ArrayList;
+
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /*
 Задача 2
@@ -13,9 +16,13 @@ import java.util.List;
  */
 public class Task2 {
 
-  public static List<Person> combineAndSortWithLimit(Collection<Person> persons1,
-                                                     Collection<Person> persons2,
-                                                     int limit) {
-    return new ArrayList<>();
-  }
+    public static List<Person> combineAndSortWithLimit(Collection<Person> persons1,
+                                                       Collection<Person> persons2,
+                                                       int limit) {
+        return Stream.of(persons1, persons2)
+                .flatMap(Collection::stream)
+                .sorted(Comparator.comparing(Person::getCreatedAt))
+                .limit(limit)
+                .collect(Collectors.toList());
+    }
 }
