ef5d041435c50f3ddc09c60893f147f44a26a46e
Korneev Serge <sakorneev90@gmail.com>
2022-12-08 15:21:34 +0300

    Task7. Shorter version

--- src/main/java/tasks/Task7.java
+++ src/main/java/tasks/Task7.java
@@ -14,9 +14,7 @@ public class Task7 {
 
   public static Set<String> vacancyNames(Collection<Company> companies) {
     return companies.stream()
-            .map(Company::getVacancies)
-            .flatMap(Collection::stream)
-            .map(Vacancy::getTitle)
+            .flatMap(company -> company.getVacancies().stream().map(Vacancy::getTitle))
             .collect(Collectors.toSet());
   }
 
