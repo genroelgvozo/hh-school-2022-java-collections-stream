96f6d7b9573360d223e7014539ce502b3726527e
bannovdaniil <baddan@mail.ru>
2022-12-08 17:25:07 +0300

    task6: Имеются: - коллекция персон Collection<Person> - словарь Map<Integer, Set<Integer>>, сопоставляющий каждой персоне множество id регионов - коллекция всех регионов Collection<Area> На выходе хочется получить множество строк вида "Имя - регион".  Если у персон регионов несколько, таких строк так же будет несколько

--- src/main/java/tasks/Task6.java
+++ src/main/java/tasks/Task6.java
@@ -8,27 +8,19 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-/*
-Имеются
-- коллекция персон Collection<Person>
-- словарь Map<Integer, Set<Integer>>, сопоставляющий каждой персоне множество id регионов
-- коллекция всех регионов Collection<Area>
-На выходе хочется получить множество строк вида "Имя - регион".
- Если у персон регионов несколько, таких строк так же будет несколько
- */
 public class Task6 {
 
     public static Set<String> getPersonDescriptions(Collection<Person> persons,
                                                     Map<Integer, Set<Integer>> personAreaIds,
                                                     Collection<Area> areas) {
-        Map<Integer, Area> areaMap = areas.stream()
-                .collect(Collectors.toMap(Area::getId, area -> area));
 
         return persons.stream()
                 .flatMap(person -> personAreaIds.get(person.getId()).stream()
-                        .filter(areaMap::containsKey)
-                        .map(areaMap::get)
-                        .map(area -> person.getFirstName() + " - " + area.getName()))
+                        .flatMap(areaId -> areas.stream()
+                                .filter(area -> areaId.equals(area.getId()))
+                                .map(areaById -> person.getFirstName() + " - " + areaById.getName())
+                        )
+                )
                 .collect(Collectors.toSet());
     }
 }
