c5aed243fbbbccb0dc1031ce01345c31f9f24636
bannovdaniil <baddan@mail.ru>
2022-12-08 16:58:47 +0300

    task6: Без стрима.

--- src/main/java/tasks/Task6.java
+++ src/main/java/tasks/Task6.java
@@ -2,23 +2,39 @@ package tasks;
 
 import common.Area;
 import common.Person;
+
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /*
 Имеются
 - коллекция персон Collection<Person>
 - словарь Map<Integer, Set<Integer>>, сопоставляющий каждой персоне множество id регионов
 - коллекция всех регионов Collection<Area>
-На выходе хочется получить множество строк вида "Имя - регион". Если у персон регионов несколько, таких строк так же будет несколько
+На выходе хочется получить множество строк вида "Имя - регион".
+ Если у персон регионов несколько, таких строк так же будет несколько
  */
 public class Task6 {
 
-  public static Set<String> getPersonDescriptions(Collection<Person> persons,
-                                                  Map<Integer, Set<Integer>> personAreaIds,
-                                                  Collection<Area> areas) {
-    return new HashSet<>();
-  }
+    public static Set<String> getPersonDescriptions(Collection<Person> persons,
+                                                    Map<Integer, Set<Integer>> personAreaIds,
+                                                    Collection<Area> areas) {
+        Map<Integer, Area> areaMap = areas.stream()
+                .collect(Collectors.toMap(Area::getId, area -> area));
+
+        Set<String> result = new HashSet<>();
+
+        for (var person : persons) {
+            var personAreas = personAreaIds.get(person.getId());
+            for (var areaId : personAreas) {
+                var area = areaMap.get(areaId);
+                result.add(person.getFirstName() + " - " + area.getName());
+            }
+        }
+
+        return result;
+    }
 }
