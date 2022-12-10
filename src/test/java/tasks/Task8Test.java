337c2076bf72612311eb6945dead404fa6f06171
Dmitrii Parfenov <parfenovds@protonmail.com>
2022-12-10 10:14:11 +0300

    add comments to Task8, add null-test and null-behaviour, methods renaming

--- src/test/java/tasks/Task8Test.java
+++ src/test/java/tasks/Task8Test.java
@@ -2,10 +2,6 @@ package tasks;
 
 import common.Person;
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.Instant;
 import java.util.*;
@@ -23,12 +19,20 @@ class Task8Test {
     private List<Person> listOfOne;
     private List<Person> listOfTwo;
     private List<Person> listOfFour;
+    private List<Person> listWithNull;
+    private final Instant clock = Instant.now();
+    private final Person testPerson = new Person(15, "Misha", clock);
+    private final Person testPerson2 = new Person(25, "Kolya", clock.plusSeconds(50));
+    private final Person testPerson3 = new Person(35, "Nikita", clock.plusSeconds(90));
+    private final Person nullNamePerson = new Person(35, null, clock.plusSeconds(120));
+    private final Person nullIdPerson = new Person(null, "Nemo", clock.plusSeconds(240));
+    private final Person nullTimePerson = new Person(300, "Nobody", null);
+    private final Person strangeNamePerson = new Person(555, "null", clock.plusSeconds(90));
 
     @BeforeAll
     void initialization() {
         task8 = new Task8();
-        Instant clock = Instant.now();
-        Person testPerson = new Person(15, "Misha", clock);
+
         listOfOne = List.of(testPerson);
         listOfTwo = List.of(
                 new Person(62, "Misha", clock.plusSeconds(1)),
@@ -44,120 +48,174 @@ class Task8Test {
                         clock.plusSeconds(4)),
                 new Person(100, "Vova", clock.plusSeconds(4))
         );
+        listWithNull = new ArrayList<>() {{
+            add(testPerson);
+            add(testPerson2);
+            add(null);
+            add(testPerson3);
+            add(nullNamePerson);
+            add(nullIdPerson);
+            add(nullTimePerson);
+            add(strangeNamePerson);
+        }};
     }
 
     @Test
-    void getNamesShouldReturnEmptyListIfEmptyListSend() {
-        assertEquals(Collections.emptyList(), task8.getNames(emptyList));
+    void getPersonsFirstNamesCompleteNullTest() {
+        assertEquals(List.of("Kolya", "Nikita", "", "Nemo", "Nobody", "null"), task8.getPersonsFirstNames(listWithNull));
     }
 
     @Test
-    void getNamesShouldReturnEmptyListIfListOfOneSend() {
-        assertEquals(Collections.emptyList(), task8.getNames(listOfOne));
+    void getPersonsFirstNamesShouldReturnEmptyListIfEmptyListSend() {
+        assertEquals(Collections.emptyList(), task8.getPersonsFirstNames(emptyList));
     }
 
     @Test
-    void getNamesShouldReturnListOfOneIfListOfTwoSend() {
-        assertEquals(List.of("Vova"), task8.getNames(listOfTwo));
+    void getPersonsFirstNamesShouldReturnEmptyListIfListOfOneSend() {
+        assertEquals(Collections.emptyList(), task8.getPersonsFirstNames(listOfOne));
     }
 
     @Test
-    void getNamesShouldReturnListOfThreeIfListOfFourSend() {
-        assertEquals(List.of("Vova", "Fedor", "Vova"), task8.getNames(listOfFour));
+    void getPersonsFirstNamesShouldReturnListOfOneIfListOfTwoSend() {
+        assertEquals(List.of("Vova"), task8.getPersonsFirstNames(listOfTwo));
     }
 
     @Test
-    void getNamesShouldReturnListOfManyWithReducedSizeIfListOfManySend() {
+    void getPersonsFirstNamesShouldReturnListOfThreeIfListOfFourSend() {
+        assertEquals(List.of("Vova", "Fedor", "Vova"), task8.getPersonsFirstNames(listOfFour));
+    }
+
+    @Test
+    void getPersonsFirstNamesShouldReturnListOfManyWithReducedSizeIfListOfManySend() {
         for (int i = 0; i < 10; i++) {
             List<Person> listOfMany = Stream.generate(new PersonSupplier())
                     .limit(SIZE_OF_LIST_OF_MANY)
                     .collect(Collectors.toList());
-            assertEquals(listOfMany.size() - 1, task8.getNames(listOfMany).size());
+            assertEquals(listOfMany.size() - 1, task8.getPersonsFirstNames(listOfMany).size());
         }
     }
 
     @Test
-    void getDifferentNamesShouldReturnDistinctNames() {
-        assertEquals(Set.of("Vova", "Fedor"), task8.getDifferentNames(listOfFour));
+    void getUniqueFirstNamesCompleteNullTest() {
+        assertEquals(Set.of("", "Kolya", "null", "Nikita", "Nobody", "Nemo"), task8.getUniqueFirstNames(listWithNull));
     }
 
     @Test
-    void getDifferentNamesShouldReturnEmptySetIfListOfOneSend() {
-        assertEquals(Collections.emptySet(), task8.getDifferentNames(listOfOne));
+    void getUniqueFirstNamesShouldReturnDistinctNames() {
+        assertEquals(Set.of("Vova", "Fedor"), task8.getUniqueFirstNames(listOfFour));
     }
 
     @Test
-    void getDifferentNamesShouldNotReturnFirstName() {
-        assertFalse(task8.getDifferentNames(listOfFour).contains("Misha"));
+    void getUniqueFirstNamesShouldReturnEmptySetIfListOfOneSend() {
+        assertEquals(Collections.emptySet(), task8.getUniqueFirstNames(listOfOne));
     }
 
     @Test
-    void convertPersonToStringShouldReturnSecondNameThenFirstNameThenMiddleName() {
-        assertEquals("Ivanov Fedor Alexeevich", task8.convertPersonToString(
+    void getUniqueFirstNamesShouldNotReturnFirstName() {
+        assertFalse(task8.getUniqueFirstNames(listOfFour).contains("Misha"));
+    }
+
+    @Test
+    void getFullNameOfPersonShouldReturnEmptyStringIfAllNamesAreNull() {
+        assertEquals("", task8.getFullNameOfPerson(nullNamePerson));
+    }
+
+    @Test
+    void getFullNameOfPersonShouldReturnSecondNameThenFirstNameThenMiddleName() {
+        assertEquals("Ivanov Fedor Alexeevich", task8.getFullNameOfPerson(
                         new Person(1, "Fedor", "Ivanov", "Alexeevich", Instant.now())
                 )
         );
     }
 
     @Test
-    void convertPersonToStringShouldReturnSecondNameThenFirstName() {
-        assertEquals("Ivanov Fedor", task8.convertPersonToString(
+    void getFullNameOfPersonShouldReturnSecondNameThenFirstName() {
+        assertEquals("Ivanov Fedor", task8.getFullNameOfPerson(
                         new Person(1, "Fedor", "Ivanov", Instant.now())
                 )
         );
     }
 
     @Test
-    void convertPersonToStringShouldReturnFirstName() {
-        assertEquals("Fedor", task8.convertPersonToString(
+    void getFullNameOfPersonShouldReturnFirstName() {
+        assertEquals("Fedor", task8.getFullNameOfPerson(
                         new Person(1, "Fedor", Instant.now())
                 )
         );
     }
 
     @Test
-    void getPersonNames() {
+    void getPersonIdToFullNameWithNull() {
+        Map<Integer, String> idToFullName = new HashMap<>() {{
+            put(null, "Nemo");
+            put(35, "Nikita");
+            put(25, "Kolya");
+            put(555, "null");
+            put(300, "Nobody");
+            put(15, "Misha");
+        }};
+        assertEquals(idToFullName, task8.getPersonIdToFullName(listWithNull));
+    }
+    @Test
+    void getPersonIdToFullName() {
         Map<Integer, String> idToFullName = new HashMap<>() {{
             put(100, "Vova");
             put(15, "Misha");
             put(91, "Petrov Vova");
             put(47, "Ivanov Fedor Alexeevich");
         }};
-        assertEquals(idToFullName, task8.getPersonNames(listOfFour));
+        assertEquals(idToFullName, task8.getPersonIdToFullName(listOfFour));
     }
 
     @Test
-    void hasSamePersonsWithoutMatches() {
-        assertFalse(task8.hasSamePersons(listOfFour, listOfTwo));
+    void haveIntersectionNull() {
+        assertFalse(task8.haveIntersection(listOfFour, null));
+        assertFalse(task8.haveIntersection(null, listOfFour));
+        assertFalse(task8.haveIntersection(null, null));
     }
 
     @Test
-    void hasSamePersonsWithMatches() {
-        assertTrue(task8.hasSamePersons(listOfOne, listOfFour));
+    void haveIntersectionWithoutMatches() {
+        assertFalse(task8.haveIntersection(listOfFour, listOfTwo));
     }
 
     @Test
-    void hasSamePersonsEmptyLists() {
-        assertFalse(task8.hasSamePersons(List.of(), List.of()));
+    void haveIntersectionWithMatches() {
+        assertTrue(task8.haveIntersection(listOfOne, listOfFour));
     }
 
     @Test
-    void countEvenPositive() {
-        assertEquals(4, task8.countEven(Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9)));
+    void haveIntersectionEmptyLists() {
+        assertFalse(task8.haveIntersection(List.of(), List.of()));
     }
 
     @Test
-    void countEvenNegative() {
-        assertEquals(4, task8.countEven(Stream.of(-1, -2, -3, -4, -5, -6, -7, -8, -9)));
+    void countEvenIntegersParallelPositiveEvenOnly() {
+        assertEquals(4, task8.countEvenIntegersParallel(Stream.of(2, 4, 6, 8)));
     }
 
     @Test
-    void countEvenZero() {
-        assertEquals(1, task8.countEven(Stream.of(0)));
+    void countEvenIntegersParallelOddOnly() {
+        assertEquals(0, task8.countEvenIntegersParallel(Stream.of(1, 3, 5, 7)));
     }
 
     @Test
-    void countEvenMixed() {
-        assertEquals(5, task8.countEven(Stream.of(-1000, -800, -99, 0, 15, 44, 88)));
+    void countEvenIntegersParallelositive() {
+        assertEquals(4, task8.countEvenIntegersParallel(Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9)));
+    }
+
+    @Test
+    void countEvenIntegersParallelNegative() {
+        assertEquals(4, task8.countEvenIntegersParallel(Stream.of(-1, -2, -3, -4, -5, -6, -7, -8, -9)));
+    }
+
+    @Test
+    void countEvenIntegersParallelZero() {
+        assertEquals(1, task8.countEvenIntegersParallel(Stream.of(0)));
+    }
+
+    @Test
+    void countEvenIntegersParallelMixed() {
+        assertEquals(5, task8.countEvenIntegersParallel(Stream.of(-1000, -800, -99, 0, 15, 44, 88)));
     }
 }
