85421577b2033f75170af38c1c9ca7b74745d95f
Igor Ishtubaev <ig.isht@gmail.com>
2022-12-24 00:29:52 +0300

    first attempt and checking git action

--- src/main/java/tasks/Task6.java
+++ src/main/java/tasks/Task6.java
@@ -2,10 +2,11 @@ package tasks;
 
 import common.Area;
 import common.Person;
+
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /*
 Имеются
@@ -19,6 +20,17 @@ public class Task6 {
   public static Set<String> getPersonDescriptions(Collection<Person> persons,
                                                   Map<Integer, Set<Integer>> personAreaIds,
                                                   Collection<Area> areas) {
-    return new HashSet<>();
+
+    Map<Integer, Area> AreaIdToName = areas.stream()
+            .collect(Collectors.toMap(Area::getId, area -> area));
+
+    return persons.stream()
+            .flatMap(person -> personAreaIds.get(person.getId()).stream()
+                    .map(areaId -> GetPersonAreaInfo(person, AreaIdToName.get(areaId))))
+            .collect(Collectors.toSet());
+  }
+
+  public static String GetPersonAreaInfo (Person person, Area area) {
+    return person.getFirstName() + " - " + area.getName();
   }
 }
